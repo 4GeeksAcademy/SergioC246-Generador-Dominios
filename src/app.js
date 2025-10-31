29a0a0f6c00f68d335992de12e643f6910b1463b
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-10-31 11:31:39 +0000

    Entrega final

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ year.forEach(year => {                       // Esto es el primer loop. Se utili
   month.forEach(month => {                     // Este es el segundo loop. Recorre este primero antes que el anterior. Se empieza desde abajo hacia arriba
     day.forEach(day => {
       domain.forEach(domain => {
-        console.log(b + ' ' + a + ' ' + c + ' ' + '.' + d)       // Esto es para concatenar todo los elementos
+        console.log(month + year + day + '.' + domain)       // Esto es para concatenar todo los elementos
+        console.log(`${month}${year}${day}.${domain}`)       // Esto es una forma más simplificada de concatenar las variables
       })
     })
   })
