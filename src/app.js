72d1eba0db9c8b318af417d2570689cef04853a5
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-10-31 11:14:52 +0000

    Entrega final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+let year = ['2022', '2023'];
+let month = ['Enero', 'Febrero', 'Marzo', 'Abril'];
+let day = ['Lunes', 'Martes'];
+const domain = ['com', 'es', 'dev', 'io', 'net']
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+year.forEach(year => {                       // Esto es el primer loop. Se utiliza el forEach para que recorra todo el array
+  month.forEach(month => {                     // Este es el segundo loop. Recorre este primero antes que el anterior. Se empieza desde abajo hacia arriba
+    day.forEach(day => {
+      domain.forEach(domain => {
+        console.log(b + ' ' + a + ' ' + c + ' ' + '.' + d)       // Esto es para concatenar todo los elementos
+      })
+    })
+  })
+})
