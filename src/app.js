ef341d878a5b27bb9b304611ae1ba2e0c0623ef3
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-03 11:36:38 +0000

    Intentando averiguar como resolver el ejercicio extra

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-let year = ['2022', '2023'];
-let month = ['Enero', 'Febrero', 'Marzo', 'Abril'];
-let day = ['Lunes', 'Martes'];
+const year = ['2022', '2023'];
+const month = ['Enero', 'Febrero', 'Marzo', 'Abril'];
+const day = ['Lunes', 'Martes'];
 const domain = ['com', 'es', 'dev', 'io', 'net']
 
 year.forEach(year => {                       // Esto es el primer loop. Se utiliza el forEach para que recorra todo el array
   month.forEach(month => {                     // Este es el segundo loop. Recorre este primero antes que el anterior. Se empieza desde abajo hacia arriba
     day.forEach(day => {
       domain.forEach(domain => {
-        console.log(`${month}${year}${day}.${domain}`)       // Esto es una forma más simplificada de concatenar las variables
+        console.log(`${year}${month}${day}.${domain}`)       // Esto es una forma más simplificada de concatenar las variables
       })
     })
   })
