969eb32374b6c62010df08b95617aa8bca28e01a
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-04 14:41:21 +0000

    Nuevo ejercicio

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const year = ['2022', '2023'];
-const month = ['Enero', 'Febrero', 'Marzo', 'Abril'];
-const day = ['Lunes', 'Martes'];
-const domain = ['com', 'es', 'dev', 'io', 'net']
+const years = ['2022', '2023'];
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril'];
+const days = ['Lunes', 'Martes'];
+const domains = ['com', 'es', 'dev', 'io', 'net']
 
-year.forEach(year => {                       // Esto es el primer loop. Se utiliza el forEach para que recorra todo el array
-  month.forEach(month => {                     // Este es el segundo loop. Recorre este primero antes que el anterior. Se empieza desde abajo hacia arriba
-    day.forEach(day => {
-      domain.forEach(domain => {
-        console.log(`${year}${month}${day}.${domain}`)       // Esto es una forma más simplificada de concatenar las variables
+domains.forEach(domain => {                       // Esto es el primer loop. Se utiliza el forEach para que recorra todo el array
+  months.forEach(month => {                     // Este es el segundo loop. Recorre este primero antes que el anterior. Se empieza desde abajo hacia arriba
+    days.forEach(day => {
+      years.forEach(year => {
+        if (domain === 'es' || day === 'Lunes')
+        console.log(`${year}${month}${day}.${domain}`)
       })
     })
   })
