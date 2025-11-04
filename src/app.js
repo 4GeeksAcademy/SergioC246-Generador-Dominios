a32eeaf42c58d652410e66fdb8548d2575974705
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-04 16:42:47 +0000

    Entrega con la cabeza explotada

--- src/app.js
+++ src/app.js
@@ -7,8 +7,11 @@ domains.forEach(domain => {                       // Esto es el primer loop. Se 
   months.forEach(month => {                     // Este es el segundo loop. Recorre este primero antes que el anterior. Se empieza desde abajo hacia arriba
     days.forEach(day => {
       years.forEach(year => {
-        if (domain === 'es' || day === 'Lunes')
-        console.log(`${year}${month}${day}.${domain}`)
+        let dayModificado = day;   // Acá estamos generando una nueva varible para poder modificar la nueva day (generada en el loop) los días Lunes y Martes
+        if (domain === "es" && (day === 'Lunes' || day === 'Martes')) { // En esta linea estamos generando un conidicional en donde decimos que si el domain es estricto igual a es y (&&) day es estricto igual a Lunes o martes.....
+          dayModificado = day.slice(0, -2) // Se elima las últimas dos letras del día. (SE ME EXPLOTÓ LA CABEZA)
+        }
+        console.log(`${year}${month}${dayModificado}.${domain}`)
       })
     })
   })
